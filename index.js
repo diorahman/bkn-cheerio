e540c2217f6d96fb0d3e77e45c00b865ca3dcf8d
Dhi Aurrahman <diorahman@rockybars.com>
2015-07-30 21:10:10 +0700

    Fix parsing mechanism

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
-var request = require ('superagent');
+var request = require ('request');
 var cheerio = require ('cheerio');
-var URL = 'http://www.bkn.go.id/in/pastikan-data-anda-benar.html';
+var URL = 'http://www.bkn.go.id/profil-pns';
 
-function parse (html){
+function parse (html) {
   var $ = cheerio.load(html);
-  var next = false;
-  var prev = '';
   var obj = {};
-  $('td[valign="top"]').each(function(){
-    var str = $(this).text().trim();
-    if (str != ':' && str.length > 0) {
-      next = false;
-      if (!prev) {
-        prev = str;
-      } else {
-        obj[prev] = str;
-        prev = '';
-      }
-    } else if (str == ':') {
-      next = true;
-    }
+  $('.pns-row').each(function(){
+    var label = $(this).find('.label').text().trim();
+    var value = $(this).find('.value').text().trim();
+    obj[label] = value.split(':').pop().trim();
   });
   return obj;
 }
 
-module.exports = function(number, cb){
-  request
-  .post(URL)
-  .set ('Content-type', 'application/x-www-form-urlencoded')
-  .send({ id : number })
-  .end(function(err, res){
-    if (err) return cb(err);
-    if (res.status != 200) return cb(new Error(res.status));
-    cb(err, parse(res.text));
+module.exports = function(number, cb) {
+  var options = {
+    url: URL,
+    method: 'POST',
+    body: 'nip=' + number,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  };
+  request(options, function(err, res, data){
+    if (err)
+      return cb(err);
+    if (res.statusCode != 200)
+      return cb(new Error('Something is wrong'));
+    cb(null, parse(data));
   });
 }
